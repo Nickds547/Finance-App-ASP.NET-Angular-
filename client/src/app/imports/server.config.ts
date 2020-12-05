6c7543ba07f21847bbda752037fd0a96c964a6dc
Nickds54 <nicksalter98@gmail.com>
2020-12-05 01:39:09 -0600

    added the s to https

--- client/src/app/imports/server.config.ts
+++ client/src/app/imports/server.config.ts
@@ -1,2 +1,2 @@
-export const LINK = "http://spending-tracker-api.azurewebsites.net/api/";
+export const LINK = "https://spending-tracker-api.azurewebsites.net/api/";
 export const HEADERS = { 'content-type': 'application/json'}  
